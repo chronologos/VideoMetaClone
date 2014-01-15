10901330e133afede4483fefafd2de6f75f6bbc8
chronologos <yiyantay+gh@gmail.com>
2014-01-15 18:10:35 +0800

    Removed unnecessary code

--- video_tagger.py
+++ video_tagger.py
@@ -3,17 +3,10 @@ import subprocess
 import os.path
 from datetime import datetime
 
-def findDate(f):
-  "Return the date/time on which the given file was created."
-
-  cDate = subprocess.check_output(['sips', '-g', 'creation', f])
-  cDate = cDate.split('\n')[1].lstrip().split(': ')[1]
-  return datetime.strptime(cDate, "%Y:%m:%d %H:%M:%S")
-
-
 sourceDir = os.getcwd() #Get current directory
 mtsfiles = []
 mp4files = []
+
 for root, dirs, files in os.walk(sourceDir,True,None,True):
     for filename in files:
     	if filename[-4:] == '.MTS':
@@ -21,7 +14,6 @@ for root, dirs, files in os.walk(sourceDir,True,None,True):
     	elif filename[-4:] == '.mp4':
     		mp4files.append(filename)
 #^^^ Get two lists of mts and mp4 files
-print mtsfiles, '//', mp4files
 
 for mts in mtsfiles:
 	for mp4 in mp4files:
